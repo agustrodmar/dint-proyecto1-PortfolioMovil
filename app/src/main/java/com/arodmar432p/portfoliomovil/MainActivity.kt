713e2ff8f05c14fe0da483e36a9fa2a60c1f264b
Agustrodmar <agustrodmar@gmail.com>
2024-01-12 01:04:55 +0100

    He trabajado sobre la segunda pantalla. He añadido el cajón del texto, sobre el que todavía estoy trabajando. Debo disminuir el tamaño del muñeco, así como el de saturno, tengo además que trabajar la navegación horizontal con las otras pantallas de Abouut me. Además he creado la pantalla de  Teconologías que uso.

--- app/src/main/java/com/arodmar432p/portfoliomovil/MainActivity.kt
+++ app/src/main/java/com/arodmar432p/portfoliomovil/MainActivity.kt
@@ -1,8 +1,11 @@
+@file:Suppress("SpellCheckingInspection")
+
 package com.arodmar432p.portfoliomovil
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -13,15 +16,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.arodmar432p.portfoliomovil.ui.theme.PortfolioMovilTheme
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.ComposableTargetMarker
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -31,7 +40,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -110,7 +118,7 @@ fun PantallaPrincipal() {
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.star),
-                    contentDescription = "image description",
+                    contentDescription = "small white stars",
                     modifier = Modifier
                         .width(22.dp)
                         .height(10.22593.dp)
@@ -122,7 +130,7 @@ fun PantallaPrincipal() {
 
 @Preview(showBackground = true)
 @Composable
-fun AboutMe(){
+fun AboutMe() {
     Box(
         modifier = Modifier
             .width(393.dp)
@@ -135,7 +143,94 @@ fun AboutMe(){
                 )
             )
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.planeta1),
+            contentDescription = "image description",
+            contentScale = ContentScale.None,
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(30.dp)
+                .width(40.99996.dp)
+                .height(44.23907.dp)
+        )
 
+        Column(
+            modifier = Modifier.align(Alignment.TopCenter),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.objetivos),
+                contentDescription = "image description",
+                modifier = Modifier
+                    .padding(top = 30.dp)
+                    .width(119.dp)
+                    .height(147.dp)
+            )
+
+            Text(
+                text = "OBJETIVOS",
+                style = TextStyle(
+                    fontSize = 25.76.sp,
+                    lineHeight = 18.89.sp,
+                    fontFamily = FontFamily(Font(R.font.bebasneue)),
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFFFFCB14),
+                    textAlign = TextAlign.Center,
+                    letterSpacing = 1.07.sp,
+                ),
+                modifier = Modifier
+                    .width(133.14343.dp)
+                    .height(39.dp)
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(45.dp)
+                .width(80.dp)
+                .height(65.89185.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.planeta2),
+                contentDescription = "Saturn picture",
+                contentScale = ContentScale.None
+            )
+        }
+
+        // El box de la caja con el texto
+        Box(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(top = 120.dp)
+                .width(274.dp)
+                .height(547.dp)
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(Color(0xFF48194D), Color(0xFF48174D00)),
+                        startY = 0.0f,
+                        endY = Float.POSITIVE_INFINITY
+                    ),
+                    shape = RoundedCornerShape(1.dp) // Ajustar este valor para cambiar la redondez de las esquinas
+                )
+                .border(
+                    width = 2.dp,
+                    color = Color(0xFF7C1DF5),
+                    shape = RoundedCornerShape(10.dp) // la forma del borde me debe coincidir con la forma del fondo
+                )
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.CenterVertically),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
+            }
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Technologies(){
 
 }
